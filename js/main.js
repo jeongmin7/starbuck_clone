28261b54c87f99ee6939fb0751c6fde306c2c11c
SEBFull-Jeongmin <lee.jeongmin1625@gmail.com>
2022-05-11 23:52:43 +0900

    프로모션 부분 슬라이드 활용 프로모션부분

--- js/main.js
+++ js/main.js
@@ -52,3 +52,33 @@ new Swiper(".notice-line .swiper-container", {
   autoplay: true,
   loop: true,
 });
+
+new Swiper(".promotion .swiper-container", {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  pagination: {
+    el: ".promotion .swiper-pagination", //페이지 번호 요소 선택자
+    clickable: true, //사용자의 페이지 번호 요소 제어여부
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+});
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener("click", function () {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    //숨김처리
+    promotionEl.classList.add("hide");
+  } else {
+    promotionEl.classList.remove("hide");
+  }
+});
