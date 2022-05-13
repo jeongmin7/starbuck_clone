1e82cf8bb732c36234f6a346fe668e2ca032f222
SEBFull-Jeongmin <lee.jeongmin1625@gmail.com>
2022-05-13 18:13:19 +0900

    css 완성git checkout css

--- js/main.js
+++ js/main.js
@@ -14,11 +14,11 @@ searchInputEl.addEventListener("blur", function () {
 });
 
 const badgeEl = document.querySelector("header .badges");
+const toTopEl = document.querySelector("#to-top");
 
 window.addEventListener(
   "scroll",
   _.throttle(function () {
-    console.log(window.scrollY);
     if (window.scrollY > 500) {
       //배지 숨기기
       // badgeEl.style.display = "none";
@@ -27,6 +27,10 @@ window.addEventListener(
         opacity: 0,
         display: "none",
       });
+      //버튼보이기!
+      gsap.to(toTopEl, 0.2, {
+        x: 0,
+      });
     } else {
       //배지 보여주기
       // badgeEl.style.display = "block";
@@ -34,11 +38,19 @@ window.addEventListener(
         opacity: 1,
         display: "block",
       });
+      gsap.to(toTopEl, 0.2, {
+        x: 100,
+      });
     }
+    //버튼숨기기!
   }, 300)
 );
 //_.throttle(함수, 시간 )
-
+toTopEl.addEventListener("click", function () {
+  gsap.to(window, 0.7, {
+    scrollTo: 0, //스크롤의 위치를 화면의 위치를 0px위치로 옮겨주겠다.
+  });
+});
 const fadeEls = document.querySelectorAll(".visual .fade-in");
 fadeEls.forEach(function (fadeEl, idx) {
   gsap.to(fadeEl, 1, {
@@ -46,6 +58,7 @@ fadeEls.forEach(function (fadeEl, idx) {
     opacity: 1,
   });
 });
+//new Swiper
 
 new Swiper(".notice-line .swiper-container", {
   direction: "vertical",
@@ -70,6 +83,18 @@ new Swiper(".promotion .swiper-container", {
     nextEl: ".promotion .swiper-next",
   },
 });
+
+new Swiper(".awards .swiper-container", {
+  autoplay: true,
+  loop: true,
+  slidesPerView: 5,
+
+  navigation: {
+    prevEl: ".awards .swiper-prev",
+    nextEl: ".awards .swiper-next",
+  },
+});
+
 const promotionEl = document.querySelector(".promotion");
 const promotionToggleBtn = document.querySelector(".toggle-promotion");
 let isHidePromotion = false;
@@ -102,3 +127,16 @@ function floatingObject(selector, delay, size) {
 floatingObject(".floating1", 1, 15);
 floatingObject(".floating2", 0.5, 15);
 floatingObject(".floating3", 1.5, 20);
+
+const spyEls = document.querySelectorAll("section.scroll-spy");
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({
+    triggerElement: spyEl, //보여짐 여부를 감시할 요소를
+    triggerHook: 0.8, //뷰포트가 시작하는 부분을 0 마지막부분을 1로 두고 0.8부분에 왔을 때 trigger
+  })
+    .setClassToggle(spyEl, "show") //show라는 class
+    .addTo(new ScrollMagic.Controller());
+});
+
+const thisYear = document.querySelector(".this-year");
+thisYear.textContent = new Date().getFullYear();
